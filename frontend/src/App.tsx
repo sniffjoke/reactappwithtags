e2f18ed8d9387b4fcb0736deae8d378f9b227c6a
sniffjoke <sniffjoke@gmail.com>
2023-12-06 11:53:44 +0300

    init project add backend and frontend add tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="py-2 px-4 shadow-2xl bg-amber-500 flex justify-center text-2xl border-2 border-amber-950 m-2">
+      12345
+    </div>
+  );
+}
+
+export default App;
